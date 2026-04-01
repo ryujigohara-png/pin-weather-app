05b0743a8665dd4b74515bab5433c61d41907b39
ryujigohara-png <ryuji.gohara@gmail.com>
2026-04-01 19:00:21 +0900

    PWABuilder診断対応版

--- static/service-worker.js
+++ static/service-worker.js
@@ -2,10 +2,20 @@
 // PWA サービスワーカー (最小構成：インストールを有効にするため)
 // ======================================================================================
 const CACHE_NAME = 'pin-weather-cache-v1';
+const ASSETS_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  './css/style.css',
+  './js/main.js'
+];
 
 // インストール時
 self.addEventListener('install', (event) => {
   console.log('Service Worker: Installing...');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
 });
 
 // アクティベート時
